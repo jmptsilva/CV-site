95be2974f936c53e50656123e21d6785c38ac79c
João Silva <jomptsilva@gmail.com>
2023-01-12 23:22:48 +0100

    Inserted navbar link options
    Inserted divs within navbar to configure styles and added some options

--- components/common/navbar.jsx
+++ components/common/navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <>
-        <div className='navbar'>
-
+  return (
+    <>
+      <div className="navbar">
+        <div className="nav-options">
+          <div>About me</div>
+          <div>Projects</div>
+          <div>Education</div>
+          <div>Skills</div>
+          <div>Experience</div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
