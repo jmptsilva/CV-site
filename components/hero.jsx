4b0d3f5164c5c3f56e4c1c087635d69864df48b9
João Silva <jomptsilva@gmail.com>
2023-01-13 16:24:53 +0100

    Created hero (structure)
    Create the hero component with the element structure and included the import for fonts

---
+++ components/hero.jsx
@@ -0,0 +1,29 @@
+import { Varela_Round } from "@next/font/google";
+import { Nunito } from "@next/font/google";
+
+
+// Fonts
+const varelaRound = Varela_Round({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const nunito = Nunito();
+
+// Default export
+export default function Hero() {
+    return (
+      <>
+        {/* sm: flex column >md: flex row */}
+        <div className="hero container mx-auto">
+            <div className="hero-photo">
+                {/* <img src="" alt="João Silva photo" /> */}
+            </div>
+            <div className="hero-details">
+                <div className="hero-name">João Silva</div>
+                <div className="hero-jobtitle">Full Stack Web Developer</div>
+            </div>
+        </div>
+      </>
+    );
+  }
