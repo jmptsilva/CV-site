d9edd71b60ae25448c8ff971a71bd9d106615435
João Silva <jomptsilva@gmail.com>
2023-01-13 15:21:47 +0100

    Inserted google fonts; Inserted containers and margins for sections
    Inserted the fonts Varela Round and Nunito through font optimization from Next.js;
    Fixed the className conflicts created by the coexistance of props and class string to Tailwind;
    Added the correct font for each element in the navbar.

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,12 +1,12 @@
-import Navbar from './common/navbar';
-import Footer from './common/footer';
+import Navbar from "./common/navbar";
+import Footer from "./common/footer";
 
-export default function Layout({children}) {
-    return (
-        <>
-        <Navbar/>
-            <main>{children}</main>
-        <Footer/>
-        </>
-    )
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <main className="container mx-auto">{children}</main>
+      <Footer />
+    </>
+  );
 }
