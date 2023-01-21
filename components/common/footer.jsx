3f88a964e38589a5783a85656bb6b8399ec3a5b3
João Silva <jomptsilva@gmail.com>
2023-01-21 14:28:28 +0100

    Inserted the components in the index page; added footer content
    Added the components to establish the correct order in the index page; Added some of the contents in the footer.

--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import { Varela_Round } from "@next/font/google";
+import { Nunito } from "@next/font/google";
+
+const varelaRound = Varela_Round({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const nunito = Nunito();
 
 export default function Footer() {
-    return (
-        <>
-        <p>Test footer</p>
-        </>
-    );
+  return (
+    <>
+      <div className="footer-author">João Silva</div>
+      <div className="footer-date">January, 2023</div>
+      {/*       <div className="footer-designed_by"></div>
+       */}{" "}
+    </>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import Hero from "../components/hero";
+import Skills from "../components/skills";
+import Education from "../components/education";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Skills />
+      <Education />
     </>
   );
 }
